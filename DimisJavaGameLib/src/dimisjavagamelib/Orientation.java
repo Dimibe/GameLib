1f22cbca481e0a5e121ff16f450fa03a19481a1e
Dimibe <dimitribegnis@gmail.com>
2015-11-07 20:24:21 +0100

    Version 3.2

---
+++ DimisJavaGameLib/src/dimisjavagamelib/Orientation.java
@@ -0,0 +1,21 @@
+package dimisjavagamelib;
+
+public enum Orientation {
+	LEFT(-1, 0), RIGHT(1, 0), UP(0, -1), DOWN(0, 1);
+
+	private int x;
+	private int y;
+
+	private Orientation(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}
