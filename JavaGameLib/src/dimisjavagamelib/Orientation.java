534aad833da0e4887a68a58efc31a93d6341c444
Dimibe <dimitribegnis@gmail.com>
2016-02-13 22:03:00 +0100

    renamed to JavaGameLib

---
+++ JavaGameLib/src/dimisjavagamelib/Orientation.java
@@ -0,0 +1,27 @@
+package dimisjavagamelib;
+
+/**
+ * Represents a standart Vector in one of each Orientations
+ * 
+ * @author dbegnis
+ *
+ */
+public enum Orientation {
+	WEST(-1, 0), EAST(1, 0), NORTH(0, -1), SOUTH(0, 1);
+
+	private int x;
+	private int y;
+
+	private Orientation(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}
---
+++ JavaGameLib/src/dimisjavagamelib/Screen.java
@@ -0,0 +1,74 @@
+package dimisjavagamelib;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferStrategy;
+import java.util.LinkedList;
+
+import dimisjavagamelib.handler.InputHandler;
+import dimisjavagamelib.interfaces.Drawable;
+
+/**
+ * The Canvas on which all the actors and the world is displayed
+ * 
+ * @author dbegnis
+ *
+ */
+public class Screen extends Canvas {
+	private static final long serialVersionUID = 1L;
+
+	private LinkedList<Drawable> drawables;
+
+	private Dimension size;
+
+	public Screen(int width, int height) {
+		drawables = new LinkedList<>();
+
+		size = new Dimension(width, height);
+
+		setPreferredSize(size);
+		setMaximumSize(size);
+		setMinimumSize(size);
+
+		addKeyListener(new InputHandler());
+		addMouseListener(new InputHandler());
+		addMouseMotionListener(new InputHandler());
+	}
+
+	public void repaint() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(2);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		draw(g2d);
+		g.dispose();
+		bs.show();
+
+	}
+
+	private void draw(Graphics2D g) {
+		g.clearRect(0, 0, size.width, size.width);
+		for (int i = 0; i < drawables.size(); i++) {
+			drawables.get(i).draw(g);
+		}
+	}
+
+	public void addDrawable(Drawable drawable) {
+		drawables.add(drawable);
+	}
+
+	public void removeDrawable(Drawable drawable) {
+		drawables.remove(drawable);
+	}
+
+	public Dimension getSize() {
+		return size;
+	}
+}
